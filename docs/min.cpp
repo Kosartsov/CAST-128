2c3df81ef0ca0a24a569c995761ae5c0ff8c4a28
Kosartsov <kosartsov.vi@phystech.edu>
2024-10-30 18:14:43 +0300

    Create min.cpp

---
+++ docs/min.cpp
@@ -0,0 +1,19 @@
+#include "cast128.h"
+
+int main() {
+    cast128 alg;
+
+    uint64_t key[2];
+    key[0] = 0x0123456712345678;
+    key[1] = 0x234567893456789A;
+
+    uint64_t msg = 0x0123456789ABCDEF;
+
+    uint64_t encoded_msg = alg.encrypt(msg, key);
+    uint64_t decoded_msg = alg.decrypt(encoded_msg, key);
+
+    std::cout << "------------Test 1--------------" << std::endl;
+    std::cout << "Message: 0x" << std::hex << msg << std::endl;
+    std::cout << "Encoded: 0x" << std::hex << encoded_msg << std::endl;
+    std::cout << "Decoded: 0x" << std::hex << decoded_msg << std::endl;
+}
