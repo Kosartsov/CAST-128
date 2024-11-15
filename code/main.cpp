7cf7a108f761d9bd154dbf2372391dc35a368329
Виталий <101200092+Kosartsov@users.noreply.github.com>
2024-11-15 19:26:54 +0300

    Update main.cpp

--- code/main.cpp
+++ code/main.cpp
@@ -48,15 +48,15 @@ int main() {
     key[0] = 0x0123456712345678;
     key[1] = 0x234567893456789A;
 
-    uint64_t msg = 0x0123456789ABCDEF;
+    // uint64_t msg = 0x0123456789ABCDEF;
 
-    uint64_t encoded_msg = alg.encrypt(msg, key);
-    uint64_t decoded_msg = alg.decrypt(encoded_msg, key);
+    // uint64_t encoded_msg = alg.encrypt(msg, key);
+    // uint64_t decoded_msg = alg.decrypt(encoded_msg, key);
 
-    std::cout << "------------Test 1--------------" << std::endl;
-    std::cout << "Message: 0x" << std::hex << msg << std::endl;
-    std::cout << "Encoded: 0x" << std::hex << encoded_msg << std::endl;
-    std::cout << "Decoded: 0x" << std::hex << decoded_msg << std::endl;
+    // std::cout << "------------Test 1--------------" << std::endl;
+    // std::cout << "Message: 0x" << std::hex << msg << std::endl;
+    // std::cout << "Encoded: 0x" << std::hex << encoded_msg << std::endl;
+    // std::cout << "Decoded: 0x" << std::hex << decoded_msg << std::endl;
 
     std::cout << "------------Test 2--------------" << std::endl;
     std::string message = "Hello, world";
